953ca617ada85bf22a3aac976551aed588734274
Telicia Watson <teli203@aol.com>
2020-08-20 15:28:27 -0500

    update password generator

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,39 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-function generatePassword (){var passwordLength= prompt("enter password length 8-128 character upper, lower, numeric, & special character required",)
-return "passwordLength=" +passwordLength;}
+function generatepassword(){
 
-// Write password to the #password input
-function writePassword(){
-  var password = generatePassword();
-  
-  var passwordText = document.querySelector("#password");
+  var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbers = "0123456789";
+  var special = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
 
-  passwordText.value = password;
+var lowercaseInput=document.getElementById("lowercase");
+var uppercaseInput=document.getElementById("uppercase");
+var specialInput=document.getElementById("special");
+var numbersInput=document.getElementById("numbers");
 
+var passlenght = prompt("Password length between 8-128 characters"); {
+  if (passlenght < 0 || passlenght > 128) alert("ERROR");
+}
+var userpassword = "";
+  var passwordCharSet = "";
+
+
+  if (lowercaseInput.checked){
+    passwordCharSet += lowercase
+  }
+  if (uppercaseInput.checked){
+    passwordCharSet += uppercase
+  }
+  if (numbersInput.checked){
+    passwordCharSet += numbers
+  }
+  if (specialInput.checked){
+    passwordCharSet += special
+  }
+  for (var i = 0; i < passlenght; i++) {
+    userpassword += passwordCharSet.charAt(Math.floor(Math.random() * passwordCharSet.length));
+  }
+
+document.getElementById("password").value=userpassword;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
